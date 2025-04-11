1123bbf35408db01b86d802bfa2eeb6298216df7
Lucas Ywamoto <92839243+lucasywamoto@users.noreply.github.com>
2025-04-11 15:12:06 -0400

    functional-test #3, #4

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -26,10 +26,10 @@ suite("Functional Tests", function () {
       chai
         .request(server)
         .keepOpen()
-        .get("/hello?name=xy_z")
+        .get("/hello?name=Lucas")
         .end(function (err, res) {
           assert.equal(res.status, 200);
-          assert.equal(res.text, "hello xy_z");
+          assert.equal(res.text, "hello Lucas");
           done();
         });
     });
@@ -39,23 +39,40 @@ suite("Functional Tests", function () {
         .request(server)
         .keepOpen()
         .put("/travellers")
-
+        .send({ surname: "Colombo" })
         .end(function (err, res) {
-          assert.fail();
-
+          assert.equal(res.status, 200);
+          assert.equal(res.type, "application/json");
+          assert.equal(res.body.name, "Cristoforo");
+          assert.equal(res.body.surname, "Colombo");
           done();
         });
     });
     // #4
     test('Send {surname: "da Verrazzano"}', function (done) {
-      assert.fail();
-
-      done();
+      chai
+        .request(server)
+        .keepOpen()
+        .put("/travellers")
+        .send({ surname: "da Verrazzano" })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.type, "application/json");
+          assert.equal(res.body.name, "Giovanni");
+          assert.equal(res.body.surname, "da Verrazzano");
+          done();
+        });
     });
   });
 });
 
 const Browser = require("zombie");
+Browser.site = "http://0.0.0.0:3000";
+const browser = new Browser();
+
+suiteSetup(function (done) {
+  return browser.visit("/", done);
+});
 
 suite("Functional Tests with Zombie.js", function () {
   this.timeout(5000);
