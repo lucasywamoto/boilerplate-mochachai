f814f0e3a1422b31161e2b8598bb494240e570e1
Lucas Ywamoto <92839243+lucasywamoto@users.noreply.github.com>
2025-04-11 14:32:43 -0400

    unit-test #3, #4, #5, #6, #7

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -19,18 +19,18 @@ suite("Unit Tests", function () {
     });
     // #3
     test("#isOk, #isNotOk", function () {
-      assert.fail(null, "null is falsey");
-      assert.fail("I'm truthy", "A string is truthy");
-      assert.fail(true, "true is truthy");
+      assert.isNotOk(null, "null is falsey");
+      assert.isOk("I'm truthy", "A string is truthy");
+      assert.isOk(true, "true is truthy");
     });
     // #4
     test("#isTrue, #isNotTrue", function () {
-      assert.fail(true, "true is true");
-      assert.fail(
+      assert.isTrue(true, "true is true");
+      assert.isTrue(
         !!"double negation",
         "Double negation of a truthy value is true"
       );
-      assert.fail(
+      assert.isNotTrue(
         { value: "truthy" },
         "Objects are truthy, but are not boolean values"
       );
@@ -42,26 +42,30 @@ suite("Unit Tests", function () {
   suite("Equality", function () {
     // #5
     test("#equal, #notEqual", function () {
-      assert.fail(12, "12", "Numbers are coerced into strings with ==");
-      assert.fail({ value: 1 }, { value: 1 }, "== compares object references");
-      assert.fail(6 * "2", "12");
-      assert.fail(6 + "2", "12");
+      assert.equal(12, "12", "Numbers are coerced into strings with ==");
+      assert.notEqual(
+        { value: 1 },
+        { value: 1 },
+        "== compares object references"
+      );
+      assert.equal(6 * "2", "12");
+      assert.notEqual(6 + "2", "12");
     });
     // #6
     test("#strictEqual, #notStrictEqual", function () {
-      assert.fail(6, "6");
-      assert.fail(6, 3 * 2);
-      assert.fail(6 * "2", 12);
-      assert.fail([1, "a", {}], [1, "a", {}]);
+      assert.notStrictEqual(6, "6");
+      assert.strictEqual(6, 3 * 2);
+      assert.strictEqual(6 * "2", 12);
+      assert.notStrictEqual([1, "a", {}], [1, "a", {}]);
     });
     // #7
     test("#deepEqual, #notDeepEqual", function () {
-      assert.fail(
+      assert.deepEqual(
         { a: "1", b: 5 },
         { b: 5, a: "1" },
         "The order of keys doesn't matter"
       );
-      assert.fail(
+      assert.notDeepEqual(
         { a: [5, 6] },
         { a: [6, 5] },
         "The order of array elements does matter"
